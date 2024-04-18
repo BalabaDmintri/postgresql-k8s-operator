bf74fe6f45b948e608e3e5fbe0de24edf7e2bde8
DmitriyBalaba <d.balaba@jazzserve.com>
2024-04-18 13:17:04 +0300

    test

--- tests/integration/ha_tests/helpers.py
+++ tests/integration/ha_tests/helpers.py
@@ -766,9 +766,9 @@ def get_host_path(ops_test: OpsTest, unit_name: str) -> None:
     env = os.environ
     env["KUBECONFIG"] = os.path.expanduser("~/.kube/config")
     synnet_output = subprocess.check_output(["kubectl", "get", "pv", "-o", "jsonpath='{range .items[*]}{"
-                                                                           "..spec.claimRef.name}{\"\\t\"}{"
+                                                                           ".spec.claimRef.name}{\"\\t\"}{"
                                                                            ".spec.hostPath.path}{\"\\n\"}{end}' | "
-                                                                           "egrep -i 'postgresql-k8s-0'"], env=env)
+                                                                           "egrep -i 'storage-controller-0'"], env=env)
     logger.info(f"-------------- source ------------ {synnet_output}")
     aa = synnet_output.decode("utf-8")
     logger.info(f"-------------- encode ------------ {aa}")
